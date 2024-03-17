4f55ba18ee424d1916d50488dad59bd252e2cd72
bogeychan <bogeychan@outlook.com>
2024-03-18 00:19:19 +0100

    fix: add missing type (#14)

--- src/config/formatters.ts
+++ src/config/formatters.ts
@@ -1,8 +1,8 @@
-import type { Context } from 'elysia';
-import type { LoggerOptions } from 'pino';
+import type { Context } from "elysia";
+import type { LoggerOptions } from "pino";
 
-import { serializeRequest } from './serializers';
-import type { _INTERNAL_ElysiaLoggerPluginAutoLoggingState } from '../types';
+import { serializeRequest } from "./serializers";
+import type { _INTERNAL_ElysiaLoggerPluginAutoLoggingState } from "../types";
 
 export const formatters = {
   log(object) {
@@ -14,11 +14,12 @@ export const formatters = {
           store: _INTERNAL_ElysiaLoggerPluginAutoLoggingState;
           derive: {};
           resolve: {};
+          decorator: {};
         }
       >;
 
       const log: Record<string, any> = {
-        request: context.request
+        request: context.request,
       };
 
       if (context.store.responseTime) {
@@ -30,8 +31,8 @@ export const formatters = {
       return serializeRequest(object as unknown as Request);
     }
     return object;
-  }
-} satisfies LoggerOptions['formatters'];
+  },
+} satisfies LoggerOptions["formatters"];
 
 export function isContext(object: unknown) {
   const context = object as Context;
@@ -42,4 +43,3 @@ export function isRequest(object: unknown) {
   const request = object as Request;
   return request.url && request.method;
 }
-
